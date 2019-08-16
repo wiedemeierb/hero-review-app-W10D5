100455feebdfc207428a819ebdb7a50c9f1c4eea
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-08-16 10:54:03 -0500

    initial array created, variable made, .map completed, and appended in the DOM in the return.  all within App.js

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,10 +3,45 @@ import './App.css';
 import Header from '../Header/Header';
 
 class App extends Component {
+
+  //creating an array within state component - //heroList
+  //superhero name, alias, power
+  //hard coding now, normally this data comes from server
+  state = {
+    heroList : [
+      {
+        superheroName: 'The Flash',
+        alias: 'Barry Allen',
+        power: 'Super Speed',
+        onDuty: true,
+      }
+    ],
+  }
+
   render () {
+    // can map up here to a variable
+    // javascript land up here(if/else), in return its jsx flavor javascript
+    // this.state.array within state.map( () => )
+    //.map loops over everything in array
+    //everytime through the loop we need to name the specific item --here its hero
+    let listOfHeros = this.state.heroList.map( (hero) => {
+        //need to return here some jsx thats going on DOM
+        return(
+          <div>
+            {/* //looking for item, which is hero as defined above */}
+            <h2>{hero.superheroName}</h2>
+            <p>Alias : {hero.alias}</p>
+            <p>Power : {hero.power}</p>
+          </div>
+        )
+    });
+
     return (
       <div className="App">
         <Header />
+        {/* getting state on the DOM; .map here as well if you want*/}
+        {/* need to call the variable, made above, in our return, listOfHeros */}
+        {listOfHeros}
       </div>
     )
   }
