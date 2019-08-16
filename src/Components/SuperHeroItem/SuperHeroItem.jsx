db243b1c9c7d12d0bb33a4f4715ec2bbe145c0bc
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-08-16 14:09:04 -0500

    click handlers created, true/false, and reporting on DOM

--- src/Components/SuperHeroItem/SuperHeroItem.jsx
+++ src/Components/SuperHeroItem/SuperHeroItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { log } from 'util';
 
 
 class SuperHeroItem extends Component {
@@ -7,8 +8,14 @@ class SuperHeroItem extends Component {
         unMask : true,
     };
     // need a function that sets state (click handler)
-    
-    // determine if we should render to DOM or now
+    toggleMask = () => {
+        console.log('clicked a button')
+        this.setState({ //method to change the state
+            unMask: !this.state.unMask
+        })
+    }
+
+    // determine if we should render to DOM or now -conditional rendering
     onOrOffDuty() {
         if(this.props.hero.onDuty) {
             return <p>On Duty</p>;
@@ -19,11 +26,15 @@ class SuperHeroItem extends Component {
     render() {
         return (
             <div className="hero">
+                {/* {JSON.stringify(this.state)} --will help when testing our button */}
                 {/* props.hero is defined in HeroList */}
                 <h2>{this.props.hero.superheroName}</h2>
-                <p>Alias : {this.props.hero.alias}</p>
                 <p>Power : {this.props.hero.power}</p>
-                
+                {/* created to show only if true with button click */}
+                { this.state.unMask && 
+                    <p>Alias : {this.props.hero.alias}</p>
+                }
+
                 {/* conditional rendering, example is on or off*/}
                 {/* if true, show the thing; the {} is javascript, && means do something */}
                 {/* { this.props.hero.onDuty && <p>On Duty</p>} */}
@@ -31,7 +42,8 @@ class SuperHeroItem extends Component {
                 {/* if/else show one thing or another */}
                 
                {this.onOrOffDuty()}
-               <button>Toggle Alias</button>
+               {/* start of our click handler */}
+               <button onClick={this.toggleMask}>Toggle Alias</button>
 
             </div>
         )
