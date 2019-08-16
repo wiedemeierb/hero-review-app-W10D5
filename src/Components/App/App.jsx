abf8a0b1942f5f241112902e01d8d0edecea9733
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-08-16 11:14:52 -0500

    all set up in APP.js and now moving to components for organization.  list render to DOM is working

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -7,12 +7,19 @@ class App extends Component {
   //creating an array within state component - //heroList
   //superhero name, alias, power
   //hard coding now, normally this data comes from server
+  //state is a object - properties inside of it can be anything really
   state = {
     heroList : [
       {
         superheroName: 'The Flash',
         alias: 'Barry Allen',
         power: 'Super Speed',
+        onDuty: true,
+      },
+      {
+        superheroName: 'Batman',
+        alias: 'Bryce Wayne',
+        power: 'Rich',
         onDuty: true,
       }
     ],
@@ -23,7 +30,7 @@ class App extends Component {
     // javascript land up here(if/else), in return its jsx flavor javascript
     // this.state.array within state.map( () => )
     //.map loops over everything in array
-    //everytime through the loop we need to name the specific item --here its hero
+    // every time through the loop we need to name the specific item --here its hero
     let listOfHeros = this.state.heroList.map( (hero) => {
         //need to return here some jsx thats going on DOM
         return(
@@ -35,9 +42,13 @@ class App extends Component {
           </div>
         )
     });
-
+    
+    //JSX
     return (
       <div className="App">
+        {/* {this.state} */}
+        {/* stringify takes anything that is passed to it and makes its a string. */}
+        {/* {JSON.stringify(this.state)} - passes it to the DOM in a string*/}
         <Header />
         {/* getting state on the DOM; .map here as well if you want*/}
         {/* need to call the variable, made above, in our return, listOfHeros */}
